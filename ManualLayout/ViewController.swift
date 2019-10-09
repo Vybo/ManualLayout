5066245e9bc20881534963189c40300a16e7fefa
Dan Vybiral <dan.vybiral@kiwi.com>
2019-10-09 16:37:02 +0200

    Events on button.

--- ManualLayout/ViewController.swift
+++ ManualLayout/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
 
         view = mainView
         mainView.backgroundColor = .white
+
+        setupEvents()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -28,6 +30,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @objc func buttonTapped(_ sender: UIButton) {
+        presentAlert(title: "Ouch", message: "You hit me!")
+    }
 
+    private func setupEvents() {
+        mainView.button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
+
+    private func presentAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(.init(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
