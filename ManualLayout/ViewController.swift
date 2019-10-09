e39a3d91040c67ed5f359a85d2a1abd847eee23f
Dan Vybiral <dan.vybiral@kiwi.com>
2019-10-09 15:09:22 +0200

    Initial Commit

---
+++ ManualLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManualLayout
+//
+//  Created by Dan Vybiral on 09/10/2019.
+//  Copyright © 2019 Kiwi.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
